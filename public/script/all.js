bc5cf1e062c84195b779be979d96a14bd6e3b17b
espenraugstad <espen.raugstad@gmail.com>
2022-04-17 22:16:14 +0200

    Removed some old code and added some comments

--- public/script/all.js
+++ public/script/all.js
@@ -1,19 +1,11 @@
-import { getCurrentUser, getUser } from "./modules/users.js";
+import { getCurrentUser } from "./modules/users.js";
 import { getAccess, checkAccess } from "./modules/access.js";
 import { logout } from "./modules/logout.js";
 import { message } from "./modules/message.js";
 
 // HTML-elements
-const user = document.getElementById('user');
-/* const followedPlaylists = document.getElementById("followedPlaylists"); */
-/* const latest = document.getElementById("latest"); */
-/* const ownedPlaylists = document.getElementById("ownedPlaylists"); */
-/* const followed = document.getElementById("followed"); */
-/* const owned = document.getElementById("owned"); */
+const user = document.getElementById('user'); 
 const copyBtn = document.getElementById("copy-button");
-/* const msg = document.getElementById("msg"); */
-
-/* const copyFrom = document.getElementById('copy-from'); */
 const copyFromLists = document.getElementById('copy-from-lists');
 const copyToLists = document.getElementById('copy-to-lists');
 const showAll = document.getElementById('show-all');
@@ -34,74 +26,18 @@ const YEAR = [
   "Dec",
 ];
 
+// Ids for playlists to copy from and to
 let copyIds = {
   from: '',
   to: '',
 }
 
-// Access token
-/* let access = null;
-
-function checkAccess() {
-  access = localStorage.getItem("access");
-  if (access && access !== "undefined") {
-    return;
-  } else {
-    window.location.href = "index.html";
-  }
-} */
-
 window.onload = function () {
   showAll.checked = false;
-  /* copyFromLists.innerHTML = '';
-  copyToLists.innerHTML = ''; */
+
   checkAccess();
   getPlaylists(0);
 };
-
-/* followed.addEventListener("change", async () => {
-  let followedTracks = await getTracks(followed.selectedOptions[0].value);
-
-  const lastTracks = 10;
-
-  let latestTracks = followedTracks.slice(-lastTracks);
-
-  let latestTracksOutput = "";
-
-  for (track of latestTracks) {
-    let added_by = await getUser(track.added_by.id);
-
-    let songName = track.track.name;
-    let artists = "";
-    for (artist of track.track.artists) {
-      artists += artist.name + ", ";
-    }
-    artists = artists.slice(0, -2);
-    let addedDate = new Date(track.added_at);
-    latestTracksOutput += `${songName} by ${artists}, added ${
-      YEAR[addedDate.getMonth()]
-    } ${addedDate.getDate()}, ${addedDate.getFullYear()}. Added by ${
-      added_by.display_name
-    }<br>`;
-  }
-  latest.innerHTML = "<h3>Last tracks added</h3>" + latestTracksOutput;
-});
-
-copyBtn.addEventListener("click", async () => {
-  checkAccess();
-  msg.innerHTML = "";
-
-  if (
-    followed.selectedOptions[0].value !== "" &&
-    owned.selectedOptions[0].value !== ""
-  ) {
-    let copyFrom = await getTracks(followed.selectedOptions[0].value);
-    let copyTo = await getTracks(owned.selectedOptions[0].value);
-    await copyList(copyFrom, copyTo, owned.selectedOptions[0].value);
-  } else {
-    msg.innerHTML = "Select two playlists";
-  }
-}); */
 
 copyBtn.addEventListener('click', async ()=>{
   
@@ -149,7 +85,8 @@ async function getPlaylists(offset) {
     let data = await res.json();
     for (let list of data.items) {
       let listId = list.id;
-      // Add list to copy from
+
+      // Add all playlists in the copy from column
       if(showAll.checked || list.owner.id !== localStorage.getItem("user")){
         let newFromDiv = document.createElement('div');
       
@@ -203,22 +140,9 @@ async function getPlaylists(offset) {
           newToDiv.classList.add('selected-playlist-to');
         });
       }
-
-      
-
-      /* let opt = document.createElement("option");
-      opt.value = list.id;
-      opt.text = `${list.name} by ${list.owner.display_name}`;
-
-      if (list.owner.id !== localStorage.getItem("user")) {
-        opt.text = `${list.name} by ${list.owner.display_name}`;
-        followed.add(opt, null);
-      } else {
-        opt.text = `${list.name}`;
-        owned.add(opt, null);
-      } */
     }
 
+    // Get more playlists if there are more than 50
     if (data.items.length >= 50) {
       getPlaylists(offset + 50);
     }
@@ -226,61 +150,6 @@ async function getPlaylists(offset) {
     console.log(err);
   }
 }
-
-/* async function getUser(user_id) {
-  const url = `https://api.spotify.com/v1/users/${user_id}`;
-
-  const cfg = {
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-      Authorization: "Bearer " + getAccess(),
-    },
-  };
-
-  try {
-    let res = await fetch(url, cfg);
-    if (res.status === 401) {
-      refresh();
-    } else if (res.status === 200) {
-      let data = await res.json();
-      return data;
-    } else if (res.status === 404) {
-      return { display_name: "Unknown" };
-    } else {
-      throw "Unable to get current user";
-    }
-  } catch (err) {
-    console.log(err);
-  }
-}
- */
-/* async function getCurrentUser() {
-  const url = "https://api.spotify.com/v1/me";
-
-  const cfg = {
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-      Authorization: "Bearer " + access,
-    },
-  };
-
-  try {
-    let res = await fetch(url, cfg);
-    if (res.status === 401) {
-      refresh();
-    } else if (res.status === 200) {
-      let data = await res.json();
-      localStorage.setItem("user", data.id);
-      currentUser.innerHTML = `${data.display_name}`;
-    } else {
-      throw "Unable to get current user";
-    }
-  } catch (err) {
-    console.log(err);
-  }
-} */
 
 async function getTracks(playlistId) {
   let max = false;
