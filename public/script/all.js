9098bf1483a3f813bd52bd5d729b62a763d9e4ad
espenraugstad <espen.raugstad@gmail.com>
2022-04-17 22:29:31 +0200

    New module to get playlists.

--- public/script/all.js
+++ public/script/all.js
@@ -2,6 +2,7 @@ import { getCurrentUser } from "./modules/users.js";
 import { getAccess, checkAccess } from "./modules/access.js";
 import { logout } from "./modules/logout.js";
 import { message } from "./modules/message.js";
+import { getPlaylists } from "./modules/playlists.js";
 
 // HTML-elements
 const user = document.getElementById('user'); 
@@ -36,7 +37,7 @@ window.onload = function () {
   showAll.checked = false;
 
   checkAccess();
-  getPlaylists(0);
+  listPlaylists(0);
 };
 
 copyBtn.addEventListener('click', async ()=>{
@@ -63,93 +64,76 @@ copyBtn.addEventListener('click', async ()=>{
 showAll.addEventListener('change', ()=>{
   copyFromLists.innerHTML = '';
   copyToLists.innerHTML = '';
-  getPlaylists(0);
+  //getPlaylists(0);
+  listPlaylists(0);
 });
 
-async function getPlaylists(offset) {
-  const url = `https://api.spotify.com/v1/me/playlists?offset=${offset}&limit=50`;
+async function listPlaylists(offset){
+  let playlists = await getPlaylists(offset);
+  for (let list of playlists.items) {
+    let listId = list.id;
 
-  const cfg = {
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-      Authorization: "Bearer " + getAccess(),
-    },
-  };
+    // Add all playlists in the copy from column
+    if(showAll.checked || list.owner.id !== localStorage.getItem("user")){
+      let newFromDiv = document.createElement('div');
+    
+      let listName = document.createElement('span');
+      let listOwner = document.createElement('span');
 
-  try {
-    let res = await fetch(url, cfg);
-    if (res.status === 401) {
-      refresh();
-    }
-    let data = await res.json();
-    for (let list of data.items) {
-      let listId = list.id;
-
-      // Add all playlists in the copy from column
-      if(showAll.checked || list.owner.id !== localStorage.getItem("user")){
-        let newFromDiv = document.createElement('div');
-      
-        let listName = document.createElement('span');
-        let listOwner = document.createElement('span');
-
-        listName.innerHTML = list.name;
-        listOwner.innerHTML = list.owner.display_name;
-        newFromDiv.appendChild(listName);
-        newFromDiv.appendChild(listOwner);
-        newFromDiv.classList.add('playlist-list');
-        copyFromLists.appendChild(newFromDiv);
-
-        
-
-        newFromDiv.addEventListener('click', (e)=>{
-          copyIds.from = listId;
-  
-          // Remove any existing selected-classes
-          let currentlySelected = document.querySelectorAll('.selected-playlist-from');
-          currentlySelected.forEach(e => e.classList.remove('selected-playlist-from'));
-  
-          // Add selected class for this particular playlist
-          newFromDiv.classList.add('selected-playlist-from');
-        });
-      }
+      listName.innerHTML = list.name;
+      listOwner.innerHTML = list.owner.display_name;
+      newFromDiv.appendChild(listName);
+      newFromDiv.appendChild(listOwner);
+      newFromDiv.classList.add('playlist-list');
+      copyFromLists.appendChild(newFromDiv);
 
 
-      // Add only owners lists to copy to
-      if(list.owner.id === localStorage.getItem("user")){
-        let newToDiv = document.createElement('div');
-      
-        let listName = document.createElement('span');
-        let listOwner = document.createElement('span');
+      newFromDiv.addEventListener('click', (e)=>{
+        copyIds.from = listId;
 
-        listName.innerHTML = list.name;
-        listOwner.innerHTML = list.owner.display_name;
-        newToDiv.appendChild(listName);
-        newToDiv.appendChild(listOwner);
-        newToDiv.classList.add('playlist-list');
-        copyToLists.appendChild(newToDiv);
+        // Remove any existing selected-classes
+        let currentlySelected = document.querySelectorAll('.selected-playlist-from');
+        currentlySelected.forEach(e => e.classList.remove('selected-playlist-from'));
 
-        newToDiv.addEventListener('click', (e)=>{
-          copyIds.to = listId;
-  
-          // Remove any existing selected-classes
-          let currentlySelected = document.querySelectorAll('.selected-playlist-to');
-          currentlySelected.forEach(e => e.classList.remove('selected-playlist-to'));
-  
-          // Add selected class for this particular playlist
-          newToDiv.classList.add('selected-playlist-to');
-        });
-      }
+        // Add selected class for this particular playlist
+        newFromDiv.classList.add('selected-playlist-from');
+      });
     }
 
-    // Get more playlists if there are more than 50
-    if (data.items.length >= 50) {
-      getPlaylists(offset + 50);
+
+    // Add only owners lists to copy to
+    if(list.owner.id === localStorage.getItem("user")){
+      let newToDiv = document.createElement('div');
+    
+      let listName = document.createElement('span');
+      let listOwner = document.createElement('span');
+
+      listName.innerHTML = list.name;
+      listOwner.innerHTML = list.owner.display_name;
+      newToDiv.appendChild(listName);
+      newToDiv.appendChild(listOwner);
+      newToDiv.classList.add('playlist-list');
+      copyToLists.appendChild(newToDiv);
+
+      newToDiv.addEventListener('click', (e)=>{
+        copyIds.to = listId;
+
+        // Remove any existing selected-classes
+        let currentlySelected = document.querySelectorAll('.selected-playlist-to');
+        currentlySelected.forEach(e => e.classList.remove('selected-playlist-to'));
+
+        // Add selected class for this particular playlist
+        newToDiv.classList.add('selected-playlist-to');
+      });
     }
-  } catch (err) {
-    console.log(err);
+  }
+
+  // List more playlists if there are more than 50
+  if (playlists.items.length >= 50) {
+    listPlaylists(offset + 50);
   }
 }
+
 
 async function getTracks(playlistId) {
   let max = false;
---
+++ public/script/modules/playlists.js
@@ -0,0 +1,25 @@
+import { getAccess } from "./access.js";
+
+export async function getPlaylists(offset) {
+    const url = `https://api.spotify.com/v1/me/playlists?offset=${offset}&limit=50`;
+  
+    const cfg = {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + getAccess(),
+      },
+    };
+  
+    try {
+      let res = await fetch(url, cfg);
+      if (res.status === 401) {
+        refresh();
+      }
+      let data = await res.json();
+      return data;
+
+    } catch(err){
+        console.log(err);
+    }
+}
